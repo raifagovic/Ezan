eaef802ac375932ad35c69257993415a5cea2c2f
Raif Agovic <raif.agovic.dev@gmail.com>
2024-03-12 10:52:20 +0100

    Initial commit

---
+++ AdhanTime/AdhanTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdhanTimeApp.swift
+//  AdhanTime
+//
+//  Created by Raif on 6. 3. 2024..
+//
+
+import SwiftUI
+
+@main
+struct AdhanTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdhanTime/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdhanTime
+//
+//  Created by Raif on 6. 3. 2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
