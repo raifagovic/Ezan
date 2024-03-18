72b97037ec2551af1baeb51b12239ea1e729420a
Raif Agovic <raif.agovic.dev@gmail.com>
2024-03-18 21:57:00 +0100

    Add Xcode files

---
+++ AdhanTime/AdhanTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdhanTimeApp.swift
+//  AdhanTime
+//
+//  Created by Raif Agovic on 18. 3. 2024..
+//
+
+import SwiftUI
+
+@main
+struct AdhanTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
