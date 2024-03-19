9632b6175931d85b9e0ba8051ea52fc9d01dd521
Raif Agovic <raif.agovic.dev@gmail.com>
2024-03-19 12:43:23 +0100

    Creaete PrayerTimeCalculator.swift

---
+++ AdhanTime/PrayerTimeCalculator.swift
@@ -0,0 +1,10 @@
+//
+//  PrayerTimeCalculator.swift
+//  AdhanTime
+//
+//  Created by Raif Agovic on 19. 3. 2024..
+//
+
+import Foundation
+
+func timeToNextPrayer(prayerTimes: [String]) -> String? {}
