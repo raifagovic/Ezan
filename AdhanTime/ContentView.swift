09bdb288f5e16e731c66f2347cf6e40c4e135fd4
Raif Agovic <raif.agovic.dev@gmail.com>
2024-03-19 15:04:34 +0100

    Define prayerTimes as a property of ContentView

--- AdhanTime/ContentView.swift
+++ AdhanTime/ContentView.swift
@@ -16,14 +16,15 @@ struct ContentView: View {
         "Banovići", "Banja Luka", "Bihać", "Bijeljina", "Bileća", "Bosanski Brod", "Bosanska Dubica", "Bosanska Gradiška", "Bosansko Grahovo", "Bosanska Krupa", "Bosanski Novi", "Bosanski Petrovac", "Bosanski Šamac", "Bratunac", "Brčko", "Breza", "Bugojno", "Busovača", "Bužim", "Cazin", "Čajniče", "Čapljina", "Čelić", "Čelinac", "Čitluk", "Derventa", "Doboj", "Donji Vakuf", "Drvar", "Foča", "Fojnica", "Gacko", "Glamoč", "Goražde", "Gornji Vakuf", "Gračanica", "Gradačac", "Grude", "Hadžići", "Han-Pijesak", "Hlivno", "Ilijaš", "Jablanica", "Jajce", "Kakanj", "Kalesija", "Kalinovik", "Kiseljak", "Kladanj", "Ključ", "Konjic", "Kotor-Varoš", "Kreševo", "Kupres", "Laktaši", "Lopare", "Lukavac", "Ljubinje", "Ljubuški", "Maglaj", "Modriča", "Mostar", "Mrkonjić-Grad", "Neum", "Nevesinje", "Novi Travnik", "Odžak", "Olovo", "Orašje", "Pale", "Posušje", "Prijedor", "Prnjavor", "Prozor", "Rogatica", "Rudo", "Sanski Most", "Sarajevo", "Skender-Vakuf", "Sokolac", "Srbac", "Srebrenica", "Srebrenik", "Stolac", "Šekovići", "Šipovo", "Široki Brijeg", "Teslić", "Tešanj", "Tomislav-Grad", "Travnik", "Trebinje", "Trnovo", "Tuzla", "Ugljevik", "Vareš", "Velika Kladuša", "Visoko", "Višegrad", "Vitez", "Vlasenica", "Zavidovići", "Zenica", "Zvornik", "Žepa", "Žepče", "Živinice", "Bijelo Polje", "Gusinje", "Nova Varoš", "Novi Pazar", "Plav", "Pljevlja", "Priboj", "Prijepolje", "Rožaje", "Sjenica", "Tutin"
     ]
     
+    let prayerTimes = ["3:38", "5:35", "12:53", "16:48", "20:08", "21:50"]
+    
     init() {
-            let prayerTimes = ["3:38", "5:35", "12:53", "16:48", "20:08", "21:50"]
-            if let timeToNext = timeToNextPrayer(prayerTimes: prayerTimes) {
-                print("Time to next prayer:", timeToNext)
-            } else {
-                print("No prayer times remaining for the day.")
-            }
+        if let timeToNext = timeToNextPrayer(prayerTimes: prayerTimes) {
+            print("Time to next prayer:", timeToNext)
+        } else {
+            print("No prayer times remaining for the day.")
         }
+    }
     
     var body: some View {
         VStack {
@@ -44,7 +45,8 @@ struct ContentView: View {
             Spacer()
             
             // Add time to next prayer UI here
-            Text("Time to Next Prayer: \(timeToNextPrayer)")
+            let timeToNextPrayerResult = timeToNextPrayer(prayerTimes: prayerTimes)
+            Text("Time to Next Prayer: \(timeToNextPrayerResult ?? "Unknown")")
                 .font(.headline)
                 .foregroundColor(.blue)
                 .padding()
