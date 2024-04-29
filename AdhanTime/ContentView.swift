4bea977cd5cb6df1e262c199128897f8bf2b50e2
Raif Agovic <raif.agovic.dev@gmail.com>
2024-04-29 15:23:33 +0200

    Create PrayerTimeAPI.swift file

--- AdhanTime/ContentView.swift
+++ AdhanTime/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
         "Banovići", "Banja Luka", "Bihać", "Bijeljina", "Bileća", "Bosanski Brod", "Bosanska Dubica", "Bosanska Gradiška", "Bosansko Grahovo", "Bosanska Krupa", "Bosanski Novi", "Bosanski Petrovac", "Bosanski Šamac", "Bratunac", "Brčko", "Breza", "Bugojno", "Busovača", "Bužim", "Cazin", "Čajniče", "Čapljina", "Čelić", "Čelinac", "Čitluk", "Derventa", "Doboj", "Donji Vakuf", "Drvar", "Foča", "Fojnica", "Gacko", "Glamoč", "Goražde", "Gornji Vakuf", "Gračanica", "Gradačac", "Grude", "Hadžići", "Han-Pijesak", "Hlivno", "Ilijaš", "Jablanica", "Jajce", "Kakanj", "Kalesija", "Kalinovik", "Kiseljak", "Kladanj", "Ključ", "Konjic", "Kotor-Varoš", "Kreševo", "Kupres", "Laktaši", "Lopare", "Lukavac", "Ljubinje", "Ljubuški", "Maglaj", "Modriča", "Mostar", "Mrkonjić-Grad", "Neum", "Nevesinje", "Novi Travnik", "Odžak", "Olovo", "Orašje", "Pale", "Posušje", "Prijedor", "Prnjavor", "Prozor", "Rogatica", "Rudo", "Sanski Most", "Sarajevo", "Skender-Vakuf", "Sokolac", "Srbac", "Srebrenica", "Srebrenik", "Stolac", "Šekovići", "Šipovo", "Široki Brijeg", "Teslić", "Tešanj", "Tomislav-Grad", "Travnik", "Trebinje", "Trnovo", "Tuzla", "Ugljevik", "Vareš", "Velika Kladuša", "Visoko", "Višegrad", "Vitez", "Vlasenica", "Zavidovići", "Zenica", "Zvornik", "Žepa", "Žepče", "Živinice", "Bijelo Polje", "Gusinje", "Nova Varoš", "Novi Pazar", "Plav", "Pljevlja", "Priboj", "Prijepolje", "Rožaje", "Sjenica", "Tutin"
     ]
     
-    let prayerTimes = ["3:38", "5:35", "12:00", "15:21", "20:08", "21:50"]
+    let prayerTimes = ["3:38", "5:35", "13:00", "15:21", "20:08", "21:50"]
         
     var body: some View {
         VStack {
---
+++ AdhanTime/PrayerTimeAPI.swift
@@ -0,0 +1,60 @@
+//
+//  PrayerTimeAPI.swift
+//  AdhanTime
+//
+//  Created by Raif Agovic on 29. 4. 2024..
+//
+
+import Foundation
+
+struct PrayerTimeAPI {
+    static let baseURL = "https://api.vaktija.ba/vaktija/v1"
+    
+    static func fetchPrayerTimes(for locationId: Int, year: Int, month: Int, day: Int, completion: @escaping (Result<[String], Error>) -> Void) {
+        let urlString = "\(baseURL)/\(locationId)/\(year)/\(month)/\(day)"
+        
+        guard let url = URL(string: urlString) else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                completion(.failure(NetworkError.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NetworkError.invalidData))
+                return
+            }
+            
+            do {
+                let prayerTimes = try parsePrayerTimes(data: data)
+                completion(.success(prayerTimes))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+    
+    static func parsePrayerTimes(data: Data) throws -> [String] {
+        let decoder = JSONDecoder()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        
+        let response = try decoder.decode(PrayerTimeResponse.self, from: data)
+        
+        // Convert date strings to formatted time strings
+        let prayerTimes = response.vakat.map { dateFormatter.string(from: $0) }
+        
+        return prayerTimes
+    }
+}
