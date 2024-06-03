f69a5d1afe195f860e480234e13bfabccad1eb06
Raif Agovic <raif.agovic.dev@gmail.com>
2024-06-03 15:35:58 +0200

    Create AppDelegate.swift file

---
+++ AdhanTime/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  AdhanTime
+//
+//  Created by Raif Agovic on 3. 6. 2024..
+//
+
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusBarController: StatusBarController?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        statusBarController = StatusBarController.shared
+        
+        // Add observer for wake notifications
+        NSWorkspace.shared.notificationCenter.addObserver(
+            self,
+            selector: #selector(macDidWake),
+            name: NSWorkspace.didWakeNotification,
+            object: nil
+        )
+    }
+    
+    @objc func macDidWake(notification: NSNotification) {
+        statusBarController?.refresh()
+    }
+}
+
