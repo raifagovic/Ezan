d4809431ec65d269eff5af40dba9b7805dabd652
Raif Agovic <raif.agovic.dev@gmail.com>
2024-06-06 15:11:11 +0200

    Adjust minutes if it equals 60

--- AdhanTime/TimeUtils.swift
+++ AdhanTime/TimeUtils.swift
@@ -5,6 +5,8 @@
 //  Created by Raif Agovic on 4. 6. 2024..
 //
 
+// TimeUtils.swift
+
 import Foundation
 
 struct TimeUtils {
@@ -12,14 +14,24 @@ struct TimeUtils {
         if interval <= 60 {
             return "\(prayerName) za \(Int(interval)) sec"
         } else {
-            let hours = Int(interval) / 3600
-            let minutes = (Int(interval) % 3600 + 59) / 60
+            let totalSeconds = Int(interval)
+            let hours = totalSeconds / 3600
+            var minutes = (totalSeconds % 3600 + 59) / 60
+            
+            // Adjust minutes if it equals 60
+            if minutes == 60 {
+                return "\(prayerName) za \(hours + 1) h"
+            }
+
             if hours > 0 {
+                // If hours and minutes are present
                 return "\(prayerName) za \(hours) h \(minutes) min"
             } else {
+                // Only minutes are present
                 return "\(prayerName) za \(minutes) min"
             }
         }
     }
 }
+
 
