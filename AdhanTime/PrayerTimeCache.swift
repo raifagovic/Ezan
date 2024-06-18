c273d272b5ba0f964666924050a644237487246a
Raif Agovic <raif.agovic.dev@gmail.com>
2024-06-18 14:00:39 +0200

    Update the savePrayerTimes function

--- AdhanTime/PrayerTimeCache.swift
+++ AdhanTime/PrayerTimeCache.swift
@@ -10,8 +10,11 @@ import Foundation
 class PrayerTimeCache {
     static let cacheKey = "cachedPrayerTimes"
     
-    static func savePrayerTimes(_ prayerTimes: [String]) {
-        UserDefaults.standard.set(prayerTimes, forKey: cacheKey)
+    static func savePrayerTimes(_ prayerTimes: [String], for date: Date) {
+        let dateKey = formattedDateKey(from: date)
+        var cachedData = UserDefaults.standard.dictionary(forKey: cacheKey) as? [String: [String]] ?? [:]
+        cachedData[dateKey] = prayerTimes
+        UserDefaults.standard.set(cachedData, forKey: cacheKey)
     }
     
     static func loadCachedPrayerTimes() -> [String]? {
