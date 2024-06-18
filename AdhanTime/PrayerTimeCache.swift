9a33285b0bc980a3f38b4964d326e5fc4ab43bbc
Raif Agovic <raif.agovic.dev@gmail.com>
2024-06-18 16:12:55 +0200

    Create the loadMonthlyCachedPrayerTimes funtion

--- AdhanTime/PrayerTimeCache.swift
+++ AdhanTime/PrayerTimeCache.swift
@@ -22,5 +22,23 @@ class PrayerTimeCache {
         let cachedData = UserDefaults.standard.dictionary(forKey: cacheKey) as? [String: [String]]
         return cachedData?[dateKey]
     }
+    
+    static func loadMonthlyCachedPrayerTimes() -> [String]? {
+        let currentMonthKey = formattedDateKeyForMonth(from: Date())
+        let nextMonthKey = formattedDateKeyForMonth(from: Calendar.current.date(byAdding: .month, value: 1, to: Date())!)
+        let cachedData = UserDefaults.standard.dictionary(forKey: cacheKey) as? [String: [String]]
+        
+        var monthlyData: [String] = []
+        
+        if let currentMonthData = cachedData?[currentMonthKey] {
+            monthlyData.append(contentsOf: currentMonthData)
+        }
+        
+        if let nextMonthData = cachedData?[nextMonthKey] {
+            monthlyData.append(contentsOf: nextMonthData.prefix(7))
+        }
+        
+        return monthlyData.isEmpty ? nil : monthlyData
+    }
 }
 
