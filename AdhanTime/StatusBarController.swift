8c0518dcce24c8b3fe714ac8366f8d2f3ce7f6a5
Raif Agovic <raif.agovic.dev@gmail.com>
2024-07-08 15:59:11 +0200

    Update the 'startTimer' function

--- AdhanTime/StatusBarController.swift
+++ AdhanTime/StatusBarController.swift
@@ -185,7 +185,7 @@ class StatusBarController {
     
     func startTimer(for interval: TimeInterval) {
         timer?.invalidate()
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateStatusBar(timer:)), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateCountdown), userInfo: nil, repeats: true)
     }
     
     @objc func updateCountdown() {
