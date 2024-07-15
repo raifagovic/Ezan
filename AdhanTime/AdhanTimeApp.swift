88a4af329ed6ff881c80ecf1b4e9a92cf69f6e8a
Raif Agovic <raif.agovic.dev@gmail.com>
2024-07-15 12:06:26 +0200

    Fix duplicate window issue by using EmptyView() in main struct

--- AdhanTime/AdhanTimeApp.swift
+++ AdhanTime/AdhanTimeApp.swift
@@ -12,8 +12,8 @@ struct AdhanTimeApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView()
         }
     }
 }
