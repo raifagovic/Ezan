9cb76f83004215f4f6e51ecda68717db1129fa92
Raif Agovic <raif.agovic.dev@gmail.com>
2024-07-15 18:59:32 +0200

    Remove the window from the AppDelegate

--- AdhanTime/AppDelegate.swift
+++ AdhanTime/AppDelegate.swift
@@ -16,23 +16,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
         statusBarController = StatusBarController.shared
         
-        // Create the main application window
-        let contentView = ContentView()
-        let window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 400, height: 600),
-            styleMask: [.titled, .closable, .resizable, .miniaturizable],
-            backing: .buffered, defer: false)
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.isReleasedWhenClosed = false // Ensure the window is not released
-        
-        // Keep a reference to the window
-        self.window = window
-        
-        // Set the main window reference in StatusBarController
-        statusBarController?.setMainWindow(window)
-        
         // Add observer for wake notifications
         NSWorkspace.shared.notificationCenter.addObserver(
             self,
