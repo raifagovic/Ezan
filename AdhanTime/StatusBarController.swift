fac2cb1ed9ccc14c41a115a4e4e132ee6991adfb
Raif Agovic <raif.agovic.dev@gmail.com>
2024-07-17 13:59:46 +0200

    Remove popover from the init

--- AdhanTime/StatusBarController.swift
+++ AdhanTime/StatusBarController.swift
@@ -18,12 +18,6 @@ class StatusBarController {
     var locationId: Int = 77 // Default locationId
     
     private init() {
-        popover = NSPopover()
-        popover.contentSize = NSSize(width: 400, height: 600)
-        popover.behavior = .transient
-        popover.animates = true
-        popover.contentViewController = NSHostingController(rootView: ContentView())
-        
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         
         if let button = statusItem.button {
