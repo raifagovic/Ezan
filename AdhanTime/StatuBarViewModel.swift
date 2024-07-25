84576d217e8739cdeb27d418606202d204bf95df
Raif Agovic <raif.agovic.dev@gmail.com>
2024-07-25 14:41:21 +0200

    Create the 'fallbackToCachedData' function

--- AdhanTime/StatuBarViewModel.swift
+++ AdhanTime/StatuBarViewModel.swift
@@ -61,4 +61,19 @@ class StatusBarViewModel: ObservableObject {
             statusItem.button?.title = "No cached data"
         }
     }
+    
+    func fallbackToCachedData() {
+        let currentDate = Date()
+        if let cachedPrayerTimes = PrayerTimeCache.loadCachedPrayerTimes(for: currentDate) {
+            if let (nextPrayerTimeInterval, nextPrayerName) = PrayerTimeCalculator.calculateRemainingTime(prayerTimes: cachedPrayerTimes) {
+                self.remainingTime = nextPrayerTimeInterval
+                self.nextPrayerName = nextPrayerName
+                let timeString = TimeUtils.formatTimeInterval(nextPrayerTimeInterval, prayerName: nextPrayerName)
+                self.statusBarTitle = timeString
+                startTimer(for: nextPrayerTimeInterval)
+            }
+        } else {
+            self.statusBarTitle = "Fetch the data!"
+        }
+    }
 }
