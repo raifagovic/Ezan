164752da27a1fa2fb6b7a390bb0f7260223dcbb8
Raif Agovic <raif.agovic.dev@gmail.com>
2024-07-25 12:54:21 +0200

    Create the 'updateStatusBar(timer: Timer)' function

--- AdhanTime/StatuBarViewModel.swift
+++ AdhanTime/StatuBarViewModel.swift
@@ -33,4 +33,32 @@ class StatusBarViewModel: ObservableObject {
         timer?.invalidate()
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateStatusBar(timer:)), userInfo: nil, repeats: true)
     }
+    
+    @objc func updateStatusBar(timer: Timer) {
+        let currentYear = Calendar.current.component(.year, from: Date())
+        let nextYear = currentYear + 1
+        
+        var prayerTimes: [String] = []
+        
+        if let currentYearTimes = PrayerTimeCache.loadYearlyCachedPrayerTimes(for: currentYear) {
+            prayerTimes.append(contentsOf: currentYearTimes)
+        }
+        
+        if Calendar.current.component(.month, from: Date()) == 12,
+           let nextYearTimes = PrayerTimeCache.loadYearlyCachedPrayerTimes(for: nextYear) {
+            prayerTimes.append(contentsOf: nextYearTimes)
+        }
+        
+        if !prayerTimes.isEmpty {
+            if let (remainingTime, nextPrayerName) = PrayerTimeCalculator.calculateRemainingTime(prayerTimes: prayerTimes) {
+                let timeString = TimeUtils.formatTimeInterval(remainingTime, prayerName: nextPrayerName)
+                statusItem.button?.title = timeString
+                self.remainingTime = remainingTime
+                self.nextPrayerName = nextPrayerName
+                startTimer(for: remainingTime)
+            }
+        } else {
+            statusItem.button?.title = "No cached data"
+        }
+    }
 }
