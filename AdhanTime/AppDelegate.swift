1bcb2ca96283aa3271410deca89e940d4a31e2f6
Raif Agovic <raif.agovic.dev@gmail.com>
2024-08-24 17:42:01 +0200

    Return AppDelegate.swift to the project

--- AdhanTime/AppDelegate.swift
+++ AdhanTime/AppDelegate.swift
@@ -2,31 +2,7 @@
 //  AppDelegate.swift
 //  AdhanTime
 //
-//  Created by Raif Agovic on 3. 6. 2024..
+//  Created by Raif Agovic on 24. 8. 2024..
 //
 
-import Cocoa
-import SwiftUI
-
-class AppDelegate: NSObject, NSApplicationDelegate {
-    
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        // Add observer for wake notifications
-        NSWorkspace.shared.notificationCenter.addObserver(
-            self,
-            selector: #selector(macDidWake),
-            name: NSWorkspace.didWakeNotification,
-            object: nil
-        )
-    }
-    
-    @objc func macDidWake(notification: NSNotification) {
-        StatusBarViewModel.shared.refresh()
-    }
-    
-    func applicationWillTerminate(_ notification: Notification) {
-        // Remove observers when the app is about to terminate
-        NSWorkspace.shared.notificationCenter.removeObserver(self)
-    }
-}
-
+import Foundation
