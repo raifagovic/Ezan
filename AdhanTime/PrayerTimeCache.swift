426bbb9c9af511b5b50666a7933a1a26657273b4
Raif Agovic <raif.agovic.dev@gmail.com>
2024-08-25 11:40:40 +0200

    Create PrayerTimeCache file

--- AdhanTime/PrayerTimeCache.swift
+++ AdhanTime/PrayerTimeCache.swift
@@ -2,14 +2,14 @@
 //  PrayerTimeCache.swift
 //  AdhanTime
 //
-//  Created by Raif Agovic on 13. 6. 2024..
+//  Created by Raif Agovic on 25. 8. 2024..
 //
 
 import Foundation
 
 class PrayerTimeCache {
     static let cacheKey = "cachedPrayerTimes"
-    
+
     static func savePrayerTimes(_ prayerTimes: [String], for date: Date) {
         let dateKey = formattedDateKey(from: date)
         var cachedData = UserDefaults.standard.dictionary(forKey: cacheKey) as? [String: [String]] ?? [:]
@@ -17,7 +17,7 @@ class PrayerTimeCache {
         UserDefaults.standard.set(cachedData, forKey: cacheKey)
         print("Saved prayer times for \(dateKey): \(prayerTimes)")
     }
-    
+
     static func loadCachedPrayerTimes(for date: Date) -> [String]? {
         let dateKey = formattedDateKey(from: date)
         let cachedData = UserDefaults.standard.dictionary(forKey: cacheKey) as? [String: [String]]
@@ -25,7 +25,7 @@ class PrayerTimeCache {
         print("Loaded cached prayer times for \(dateKey): \(data ?? [])")
         return data
     }
-    
+
     static func removeOldData(before date: Date) {
         let dateKey = formattedDateKey(from: date)
         var cachedData = UserDefaults.standard.dictionary(forKey: cacheKey) as? [String: [String]] ?? [:]
@@ -33,7 +33,7 @@ class PrayerTimeCache {
         UserDefaults.standard.set(cachedData, forKey: cacheKey)
         print("Removed old data before \(dateKey)")
     }
-    
+
     private static func formattedDateKey(from date: Date) -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
