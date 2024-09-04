62c3552c2914dc1e2f3bc3f0a5ade1a2d8891d69
Raif Agovic <raif.agovic.dev@gmail.com>
2024-09-04 18:36:59 +0200

    Update savePrayerTimes and loadCachedPrayerTimes to Use the New Cache Key

--- AdhanTime/PrayerTimeCache.swift
+++ AdhanTime/PrayerTimeCache.swift
@@ -9,31 +9,31 @@ import Foundation
 
 class PrayerTimeCache {
     static let cacheKey = "cachedPrayerTimes"
-
-    static func savePrayerTimes(_ prayerTimes: [String], for date: Date) {
-        let dateKey = formattedDateKey(from: date)
+    
+    static func savePrayerTimes(_ prayerTimes: [String], for date: Date, locationId: Int) {
+        let dateKey = formattedDateKey(from: date, locationId: locationId)
         var cachedData = UserDefaults.standard.dictionary(forKey: cacheKey) as? [String: [String]] ?? [:]
         cachedData[dateKey] = prayerTimes
         UserDefaults.standard.set(cachedData, forKey: cacheKey)
         print("Saved prayer times for \(dateKey): \(prayerTimes)")
     }
-
-    static func loadCachedPrayerTimes(for date: Date) -> [String]? {
-        let dateKey = formattedDateKey(from: date)
+    
+    static func loadCachedPrayerTimes(for date: Date, locationId: Int) -> [String]? {
+        let dateKey = formattedDateKey(from: date, locationId: locationId)
         let cachedData = UserDefaults.standard.dictionary(forKey: cacheKey) as? [String: [String]]
         let data = cachedData?[dateKey]
         print("Loaded cached prayer times for \(dateKey): \(data ?? [])")
         return data
     }
-
+    
     static func removeOldData(before date: Date) {
-        let dateKey = formattedDateKey(from: date)
+        let dateKey = formattedDateKey(from: date, locationId: nil) // No need for locationId here
         var cachedData = UserDefaults.standard.dictionary(forKey: cacheKey) as? [String: [String]] ?? [:]
         cachedData = cachedData.filter { $0.key >= dateKey }
         UserDefaults.standard.set(cachedData, forKey: cacheKey)
         print("Removed old data before \(dateKey)")
     }
-
+    
     private static func formattedDateKey(from date: Date, locationId: Int?) -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
@@ -42,5 +42,6 @@ class PrayerTimeCache {
             dateKey += "-\(locationId)"
         }
         return dateKey
-    }}
+    }
+}
 
